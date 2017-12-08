2f3e3a1cb9b167f2e0e5e747cd92c16a68d4aec1
CurmudJim <jgarvey86@gmail.com>
2017-12-08 12:21:37 -0600

    Added a homepage and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  root "pages#home"
+
+  get 'about' => 'pages#about'
+
+  get 'pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
