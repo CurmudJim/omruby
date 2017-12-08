d8aad16ebf75037db01ca9e519a2a8d8ed2dfb02
CurmudJim <jgarvey86@gmail.com>
2017-12-08 12:30:35 -0600

    Added a contact page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   get 'about' => 'pages#about'
 
-  get 'pages/contact'
+  get 'contact' => 'pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
