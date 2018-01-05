ab5b0b2cda0ff3ca24d06c1b53519ca834451130
CurmudJim <jgarvey86@gmail.com>
2018-01-05 12:33:04 -0600

    Moved active admin to top

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  
+  ActiveAdmin.routes(self)
+  devise_for :users
+  as :user do
+    get 'signin', to: 'devise/sessions#new'
+    delete 'signout', to: 'devise/sessions#destroy'
+    get 'signup', to: 'devise/registrations#new'
+  end
+
   get 'feed', to: 'feed#show'
 
   resources :users, only: :show, param: :username do
@@ -11,13 +20,7 @@ Rails.application.routes.draw do
 
   resources :beers
   resources :tweets
-  ActiveAdmin.routes(self)
-  devise_for :users
-  as :user do
-    get 'signin', to: 'devise/sessions#new'
-    delete 'signout', to: 'devise/sessions#destroy'
-    get 'signup', to: 'devise/registrations#new'
-  end
+
 
   get 'pages/home'
 
