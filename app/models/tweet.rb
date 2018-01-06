c7e7830b5676e3e8caf750a9630ad4ac432e2781
CurmudJim <jgarvey86@gmail.com>
2018-01-05 18:23:51 -0600

    Added styles

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,7 +5,12 @@ class Tweet < ApplicationRecord
   validates :user, presence: true
   validates :content, presence: true, length: {maximum: 140 }
 
-  has_attached_file :image
-  
+  has_attached_file :image, styles: {
+    thumb: '100x100>',
+    square: '200x200#',
+    medium: '300x300>'
+  }
+
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
