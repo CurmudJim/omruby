8160dca5c8082af265bdb786f152d224ac3edbef
CurmudJim <jgarvey86@gmail.com>
2018-02-16 18:47:52 -0600

    Added anchor

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -4,13 +4,13 @@ class WatchesController < ApplicationController
   def create
     movie = Movie.find(params[:id])
     movie.liked_by current_user
-    redirect_to movies_path(@movies, :anchor => "wall")
+    redirect_to movies_path(@movies, :anchor => movie.title)
   end
 
   def destroy
     movie = Movie.find(params[:id])
     movie.unliked_by current_user
-    redirect_to movies_path(@movies, :anchor => "wall")
+    redirect_to movies_path(@movies, :anchor => movie.title)
   end
 
 end
