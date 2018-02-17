db97f08eb6d2ebd1dc3a6a82ca54aa58a04f0eff
CurmudJim <jgarvey86@gmail.com>
2018-02-16 18:22:52 -0600

    Added anchor

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,26 +1,16 @@
 class WatchesController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    movies = Movie.all
-    @watched = movies.liked_by current_user
-  end
-
   def create
     movie = Movie.find(params[:id])
     movie.liked_by current_user
-    redirect_to movies_path
+    redirect_to movies_path(@movies, :anchor => "wall")
   end
-
-
 
   def destroy
     movie = Movie.find(params[:id])
     movie.unliked_by current_user
-    redirect_to movies_path
+    redirect_to movies_path(@movies, :anchor => "wall")
   end
-
-
-
 
 end
